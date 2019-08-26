ecbc87544ea1130658ec8e06450187d832506729
Elias <etrofunenko@gmail.com>
2019-08-26 21:54:53 +0300

    Added image loading from backend, product appearance updated.

---
+++ SampleOnlineStore/SampleOnlineStore/Controllers/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleOnlineStore.Controllers
+{
+    [Route("img")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+		private readonly IHostingEnvironment _appEnvironment;
+		public ImageController(IHostingEnvironment appEnvironment)
+		{
+			_appEnvironment = appEnvironment;
+		}
+
+		[HttpGet("{imgName}")]
+		public IActionResult getImage(string imgName)
+		{
+			string filePath = Path.Combine(_appEnvironment.ContentRootPath, "Images/" + imgName);
+			return PhysicalFile(filePath, "image/png");
+		}
+    }
+}
--- SampleOnlineStore/SampleOnlineStore/Frontend/src/components/products/Product.js
+++ SampleOnlineStore/SampleOnlineStore/Frontend/src/components/products/Product.js
@@ -3,15 +3,15 @@ import './Product.css'
 
 export class Product extends Component {
     
-
     render() {
         const { product } = this.props;
         return (
             <div key={product.id} className="col-sm-6 col-md-3">
                 <div className="product" >
-                    <img src={product.imageUrl} alt={product.name} />
+                    <img src={process.env.REACT_APP_DEFAULT_API_URL + '/' + product.imageUrl} alt={product.name} />
                     <div className="image_overlay"/>
-                    <div className="view_details">Add to cart</div>
+                    <div className="add_to_cart">Add to cart</div>
+                    <div className="see_more">See more</div>
                     <div className="stats">
                         <div className="stats-container">
                             <span className="product_price">{'$'+ product.price}</span>
